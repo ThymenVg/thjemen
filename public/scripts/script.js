96020317eecb2088f096f567b265f21a4ec68fa1
ThymenVg <tvangijselk@gmail.com>
2021-06-21 19:23:49 +0200

    origiin

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 const id = "text";
-repeat(0, ["thjemen", "Thymen", "TheVRuniverse", "TheVRcomrade", "pro cat thief"], [0, 128, 4096, 64]) // delays: [textDelay, typeDelay, beforeEraseDelay, eraseDelay]
+repeat(0, ["You just became unclean!"], [0, 128, 4096, 64]) // delays: [textDelay, typeDelay, beforeEraseDelay, eraseDelay]
 
 
 async function repeat(repeatDelay = 0, texts, delays) { // delays: [textDelay, typeDelay, beforeEraseDelay, eraseDelay]
@@ -28,11 +28,5 @@ async function type(text, delays) {
             if (counter < text.length) setTimeout(addLetter, (delays[0] ? delays[0] : 100) + Math.round(-10 + Math.random() * 20));
             else setTimeout(removeLetter, (delays[1] ? delays[1] : 1000));
         })();
-
-        function removeLetter() {
-            div.textContent = div.textContent.slice(0, div.textContent.length - 1);
-            if (div.textContent.length) setTimeout(removeLetter, delays[2] ? delays[2] : 40);
-            else resolve();
-        }
     });
 }
